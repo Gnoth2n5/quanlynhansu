dcab88be4193bb05056465cdeca80fd5ad504d48
RestiaGnoth <thongpmpn00089@gmail.com>
2024-11-21 08:30:09 +0700

    Update HomeController to render the signin view instead of the welcome view

--- app/Controllers/HomeController.php
+++ app/Controllers/HomeController.php
@@ -4,7 +4,6 @@ namespace App\Controllers;
 
 class HomeController extends Controller{
     public function home(){
-
-        $this->render('welcome');
+        $this->render('authentication.signin');
     }
 }
