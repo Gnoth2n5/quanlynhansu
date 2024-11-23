525bb97d1e79268502a963d769346afc335a8a7a
RizuNguyen <tungnnpn00006@gmail.com>
2024-11-23 09:42:10 +0700

    update office
    update office

---
+++ app/Controllers/Auth/AuthController.php
@@ -0,0 +1,68 @@
+<?php
+
+namespace App\Controllers\Auth;
+use App\Controllers\Controller;
+use App\Models\Users;
+use App\Helpers\Redirect;
+
+
+class AuthController extends Controller
+{
+    public function loginView()
+    {
+        return $this->render('pages.authentication.signin');
+    }
+
+    public function registerView()
+    {
+        return $this->render('pages.authentication.signup');
+    }
+
+    public function login()
+    {
+        $email = $_POST['email'];
+        $password = $_POST['password'];
+        
+        $user = Users::where('email', $email)->first();
+
+        if ($user && password_verify($password, $user->password)) {
+            $_SESSION['user'] = $user;
+            Redirect::to('/dashboard')
+                ->message('Login successfully', 'success')
+                ->send();
+        }
+
+        Redirect::to('/')
+            ->message('Tài khoản hoặc mật khẩu không chính xác', 'danger')
+            ->send();
+    }
+
+    public function register()
+    {
+        $username = $_POST['username'];
+        $email = $_POST['email'];
+        $password = $_POST['password'];
+
+        if(!$username || !$email || !$password) {
+            echo 'All fields are required';
+        }
+
+        $user = new Users();
+        $user->username = $username;
+        $user->email = $email;
+        $user->password = password_hash($password, PASSWORD_ARGON2ID);
+        $user->save();
+
+        Redirect::to('/')
+            ->message('Register successfully', 'success')
+            ->send();
+    }
+
+    public function logout()
+    {
+        session_destroy();
+        Redirect::to('/')
+            ->message('Logout successfully', 'success')
+            ->send();
+    }
+}
---
+++ src/views/pages/admin/office/office.blade.php
@@ -0,0 +1,27 @@
+<?php
+
+
+
+$columns = [
+    'id' => 'ID',
+    'name' => 'Tên',
+    'location' => 'Vị trí',
+   
+];
+?>
+
+@extends('layouts.defaultLayout')
+
+@section('title', 'Quản lý phòng ban')
+
+@section('content')
+    <div class="col-lg-12 grid-margin stretch-card">
+        <div class="card">
+            <div class="card-body">
+                <h3>Quản lý phòng ban</h3>
+                @component('components.table.table_normal', ['columns' => $columns, 'data' => $data])
+                @endcomponent
+            </div>
+        </div>
+    </div>
+@endsection
